c24b685f87a9fcc47ce0983af22e193754cfee22
root <root@damian-P5K-SE-EPU>
2014-04-14 15:42:40 +0200

    Pinch-zoom

--- DannyTest/src/com/example/dannytest/MainActivity.java
+++ DannyTest/src/com/example/dannytest/MainActivity.java
@@ -7,6 +7,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.ScrollView;
 
@@ -16,13 +17,16 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         ScrollView scv = new ScrollView(this); // Erzeugung eines ScrollViews, mit dem "gescrollt" werden kann
         CanvasTest cv = new CanvasTest(this); // Erzeugung des Canvas (Definition siehe unten)
-        cv.setMinimumHeight(800); // Festlegung der Höhe des Canvas
-        scv.addView(cv);  // Canvas wird in der ScrollView eingefügt: Ist also "scrollable".
+        cv.setMinimumHeight(800); // Festlegung der Hï¿½he des Canvas
+        scv.addView(cv);  // Canvas wird in der ScrollView eingefï¿½gt: Ist also "scrollable".
         setContentView(scv);  // Anzeige des ScrollViews mit dem darin eingebetteten Canvas
     }
 }
 
 class CanvasTest extends View{
+	
+	private ScaleGestureDetector mScaleDetector;  // Detector fï¿½r Skalierungsgesten
+	private float mScaleFactor = 1.f;  // aktueller Skalierungsfaktor fï¿½r das Icon
 
 	public CanvasTest(Context context) {
 		super(context);
@@ -44,6 +48,7 @@ class CanvasTest extends View{
 	 canvas.drawColor(Color.WHITE);
 	// BitmapFactory.Options options = new BitmapFactory.Options();
 	 canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.fewo_grundriss), 0, 0, null);
+	 canvas.scale(mScaleFactor, mScaleFactor);
 	 canvas.drawCircle(200.0f, 200.0f, 10.0f, myPaint); 
 	 canvas.drawCircle(200.0f, 450.0f, 10.0f, myPaint);
 	 canvas.drawCircle(400.0f, 550.0f, 10.0f, myPaint);
@@ -54,6 +59,16 @@ class CanvasTest extends View{
 	 
 	 }
 	
+	 private class ExampleScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+         // automatische Ausfï¿½hrung, wenn eine Skalierungsgeste entdeckt wurde
+           public boolean onScale(ScaleGestureDetector detector) {
+                 // Anpassung des Skalierungsfaktors gemï¿½ï¿½ der erkannten Geste
+                   mScaleFactor *= detector.getScaleFactor();  
+                   mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 5.0f));
+                   invalidate();
+                   return true;
+           }
+   }
 	
 	
 }
